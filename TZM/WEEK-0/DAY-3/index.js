b1499339d757bc278d45918240c7a2207db84cd1
chuiyang <chuiyang@qunhemail.com>
2022-04-01 19:28:14 +0800

    修改注释

--- TZM/WEEK-0/DAY-3/index.js
+++ TZM/WEEK-0/DAY-3/index.js
@@ -1,9 +1,12 @@
-// 所有的function都写在这就好了 函数名最好接近实际的作用 有一定的语义化 这是个好习惯
+// 以后每天的task 都这样统一写在一个文件就好了 (因为我用script标签把这个js文件连接到index.html里了) 
+// 不用一个任务建一个html~
+
 
 /**
  * for example
  */
 // task1
+//函数名最好有一定的语义化 这是个好习惯
 function add(a, b) {
   console.log(a + b);
 }
