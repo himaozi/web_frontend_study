79a687c99fa17e8b2dc23ab560bf877e8f52cc04
chuiyang <chuiyang@qunhemail.com>
2022-04-01 19:25:19 +0800

    批改作业 add 设置任务模板

---
+++ TZM/WEEK-0/DAY-3/index.js
@@ -0,0 +1,18 @@
+// 所有的function都写在这就好了 函数名最好接近实际的作用 有一定的语义化 这是个好习惯
+
+/**
+ * for example
+ */
+// task1
+function add(a, b) {
+  console.log(a + b);
+}
+
+test(1, 2);
+
+//  task2
+function minus(a, b) {
+  console.log(a - b);
+}
+
+test2(3, 1);
