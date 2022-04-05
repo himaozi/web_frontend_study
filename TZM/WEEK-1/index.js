4a27e976e83555c05125ea234815b30a244affd7
chuiyang <chuiyang@qunhemail.com>
2022-04-06 01:19:02 +0800

    布置作业

---
+++ TZM/WEEK-1/index.js
@@ -0,0 +1,17 @@
+const tableData = [
+  {
+    name: 'liming',
+    age: 23,
+    address: '大连',
+  },
+  {
+    name: '小李',
+    age: 23,
+    address: '广西',
+  },
+  {
+    name: '阿江',
+    age: 20,
+    address: '山东',
+  },
+];
