022109f481d4a857f8a1018c3b734d3b5e1a60c7
miottz <tami_2296@163.com>
2022-04-20 17:58:53 +0800

    less和promise作业提交

--- TZM/WEEK-3/DAY-0/index.js
+++ TZM/WEEK-3/DAY-0/index.js
@@ -1,16 +1,22 @@
 // 作业promise的使用
 
 //color: red / green /yellow
-function light(color){
+function light(color) {
     console.log(color + '灯亮了');
 }
 
 // 作业-0
 // 用promise 实现一个红绿灯控制函数 1秒后红灯亮 红灯亮2秒后 绿灯亮. 
-function trafficLights(){
+function trafficLights() {
+    new Promise(function (resolved, reject) {
+    setTimeout(light('red'), 1000)
+})
+.then(setTimeout(light('green'), 2000))
     // to do
 }
 
-// 作业-1 
+trafficLights()
+
+// 作业-1
 // 理解下 promise.all  promise.race promise.any 这3个方法
 // 暂不设置作业了,以后碰到了有印象就行.
