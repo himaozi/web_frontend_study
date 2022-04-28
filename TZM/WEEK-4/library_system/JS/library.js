f508067eb9c66ac0716ecc1a8abf2ce1ef7ca5e2
miottz <tami_2296@163.com>
2022-04-28 20:42:49 +0800

    图书项目部分代码提交

--- TZM/WEEK-4/library_system/JS/library.js
+++ TZM/WEEK-4/library_system/JS/library.js
@@ -1,74 +1,146 @@
+// 封装表格渲染函数
+function tableGeneration(data, pageSize, PageCurrent) {
+  $('table tbody').empty();
+  content = ''
+  for (i = 0; i < pageSize; i++) {
+    var k = i + pageSize * (PageCurrent - 1)
+    if (k < data.length) {
+      const book = data[k];
+      content += `
+          <tr>
+              <td>${book.id}</td>
+              <td>${book.type}</td>
+              <td>${book.name}</td>
+              <td>${book.description}</td>
+              <td>
+                  <button class="btn btn-primary" onClick="editBook(${book.id})">编辑</button>
+                  <button class="btn btn-primary" onClick="delBook(${book.id})">删除</button>
+              </td>
+          <tr>
+      `;
+    }
+    $('table tbody').html(content);
+
+  }
+
+}
+
+
+// 定义分页渲染函数
+function pageGeneration(data, pageSize, PageCurrent) {
+  let pageSum = Math.ceil(data.length / pageSize)
+  if (pageSize === null) {
+    pageSize = 3
+  }
+  // 根据输入的数据条数动态生成页码标签个数
+  // 01清空页码
+  $('.pagination').html('')
+  let content = ''
+  // 02生成页码标签
+  for (i = 0; i < pageSum; i++) {
+    content +=
+      `<li class="page-item"><button class="page-link" value=${i + 1}>${i + 1}</button></li> `
+  }
+  $('.pagination').html(content)
+
+  // 渲染默认状态下的表格
+  tableGeneration(data, pageSize, PageCurrent)
+  // 点击页码渲染对应分页表格
+  $('button').click(function () {
+    PageCurrent = this.value
+    console.log(PageCurrent)
+    tableGeneration(data, pageSize, PageCurrent)
+  })
+
+}
+
+
+
 // 获取所有图书，渲染成表格
 function getBooklist() {
   axios.get('/api/books').then(function (res) {
     let booklist = Array.from(res.data.data);
     // 可以加个排序
-    $('table tbody').empty();
-    // for (i = 0; i < booklist.length; i++) {
-    //   var tr = document.createElement('tr');
-    //   document.getElementById('booktable').appendChild(tr);
-    //   let index = 0;
-    //   for (var k in booklist[i]) {
-    //     var td = document.createElement('td');
-    //     tr.appendChild(td);
-
-    //     if (index < 4) {
-    //       td.innerText = booklist[i][k];
-    //     } else if (index == 4) {
-    //       var button = document.createElement('button');
-    //       td.appendChild(button);
-    //       button.innerText = '编辑';
-    //       button.onclick  = editBook;
-    //     } else if (index == 5) {
-    //       var button = document.createElement('button');
-    //       td.appendChild(button);
-    //       button.innerText = '删除';
-    //       button.id = booklist[i].id;
-    //       button.onclick  = delBook;
-    //     }
-    //     index++;
-    //   }
-    // }
-    // 这种写法属于是 es6多行字符串+ jq 的写法 你上边的写法属于是原生的写法 有点繁琐 而且不太好用bootstrap的class组件
-    let content = '';
-    for (i = 0; i < booklist.length; i++) {
-      const book = booklist[i];
-      content += `
-            <tr>
-                <td>${book.id}</td>
-                <td>${book.type}</td>
-                <td>${book.name}</td>
-                <td>${book.description}</td>
-                <td>
-                    <button class="btn btn-primary" onClick="editBook(${book.id})">编辑</button>
-                    <button class="btn btn-primary" onClick="delBook(${book.id})">删除</button>
-                </td>
-            <tr>
-        `;
-    }
-    // 这种直接用标签名在选择元素不 太合理, 比如你页面有多个table怎么办 一般用class 和 id选择器 优化下
-    $('table tbody').html(content);
+    pageGeneration(booklist, 3, 1)
   });
 }
-// 装个code spell checker插件 会提示一些命名问题
-// 一般命名都是小驼峰 小写开头 这个应该是getBookList更规范点
 getBooklist();
+
+
+
+
+
+
+//分页查询数据
+function searchBooks() {
+  // 获取输入信息
+  var book = {
+    id: '',
+    type: $('#type').val(),
+    name: $('#name').val(),
+    description: $('#description').val(),
+    creatTime: '',
+    updateTime: ''
+
+  }
+  // 确认图书信息是否已经输入
+  if (!Object.values(book).every(v => v == '')) {
+    // 有信息则发送数据
+
+    //01 获取当前页码（待完善函数）
+
+
+    // 02 发送数据
+    url = '/api/books' + '/' + currentPage + '/' + PageSize
+    axios.get(url, { Params: book }).then(function (res) {
+      let booklist = Array.from(res.data.data);
+
+
+    // 03  接收数据后渲染表格
+
+
+    });
+  }
+  else {
+    alert('请填写图书信息')
+  }
+}
+
+
 
 // 删除图书
 function delBook(id) {
   alert('你正在删除id为' + id + '的书籍');
+
 }
+
+
 // 编辑图书
 function editBook(id) {
   alert('你编辑操作id为' + id + '的书籍');
+  // 获取当前ID的图书信息
+  url = '/api/books' + '/' + id
+  axios.get(url).then(function (res) {
+    return a = res.data.data
+
+  })
+  console.log(a)
 }
+
+
+
+
+
+
+
 
 
 
 // 删除接口生效测试
 var url = '/api/books/' + '108';
-axios.delete(url).then(function(res){
-  console.log('删除success')})
+axios.delete(url).then(function (res) {
+  console.log('删除success')
+})
 
 // 编辑接口生效测试
 
@@ -107,76 +179,10 @@ axios.get(url, { Params: Params }).then(function (res) {
   console.log('id查询success');
 });
 
-// // 新增图书
-// function addbooks(data){
-//     let bookObj = {}
-//     bookObj = {
-//         type : $('#addbooks_type').val(),
-//         name : $('#addbooks_name').val(),
-//         description : $('#addbooks_description').val(),
-//         id:data.length+1
-//     }
-//     axios.post('http://106.14.162.86:8080/books',{book:bookObj}).then(function(res){
-//         console.log(res.data)
-//     })
-// }
-// let bookObj = {
-//     type : '5',
-//     name : '1',
-//     description :'test',
-//     id:999
-// }
-// axios.post('/api/books',{
-//     id:108,
-//     name:'22',
-//     type:'33'
-// }).then(function(res){
-//         console.log(res.data)
-//     })
 
-// $.get('http://www.liulongbin.top:3006/api/getbooks', function(res) {
-//         // 2. 获取列表数据是否成功
-//         if (res.status !== 200) return alert('获取图书列表失败！')
-//         // 3. 渲染页面结构
-//         var rows = []
-//         console.log(res.data)})
 
-// axios.get('/api/books')
-// .then(function (res) {
-//     $('table tbody').append('<tr></tr>')
-//     let tableData=res.data[1]
 
-// $('table tbody').append('<th scope="row">1</th><td></td><td>Otto</td><td>@mdo</td><td><button type="button" class="btn btn-primary" data-toggle="modal"data-target="#exampleModalCenter">编辑</button>  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">删除</button>')
 
-// })
 
-// $('button').click(function getTableList() {
-//     var search = {
-//         type:$('#type').val(),
-//         bookname:$('#bookname').val(),
-//         description:$('#description').val()}
-//         console.log(search)
-//     let column = 1
-//     let val = 1
-//     let url = 'http://106.14.162.86:8080/books/'+column+'/'+val
 
-//     if (search.type !== '' || search.name !== '' || search.description !== '') {
-//         let column = 1
-//         let val = 1
 
-//         axios.get(url, { Params: search })
-//             .then(function (res) {
-//                 console.log(res.data.data.records[0])
-
-//             })
-
-//     }
-
-// })
-
-// function tableUI(data){
-//     var data = {size:1}
-//     $('#table').empty()
-//     for(i=0; i<data.size;i++){
-//         $('#table').append('<tr><td>这是新增的元素<td><tr>')
-//     }
